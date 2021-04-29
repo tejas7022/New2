01802270d72bf332016ac5e07868854bea57a1b6
tejas7022 <tejashwersai@gmail.com>
2021-04-29 09:04:07 +0530

    First Commit

---
+++ src/main/scala/First.scala
@@ -0,0 +1,10 @@
+object First {
+  def main(args: Array[String]) {
+    var a = 1
+    a match{
+      case 1 => println("One")
+      case 2 => println("Two")
+      case _ => println("No")
+    }
+  }
+}  
